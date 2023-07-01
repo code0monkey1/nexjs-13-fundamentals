14c3543fbb1bdc6929ab3776ab1d623b36c10dba
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-01 12:12:46 +0000

    1 Jul 2023, 17:42

--- app/api/new-page/route.js
+++ app/api/new-page/route.js
@@ -1,4 +1,12 @@
-export default function Get(REQUEST){
 
-      get()
+
+export default function GET(request){
+
+    const users=[
+      {id:1,name:"chinu"},
+      {id:2,name:"vonn"},
+      {id:3,name:"veronicca"}
+    ]
+
+    return new Response(JSON.stringify(users))
 }
