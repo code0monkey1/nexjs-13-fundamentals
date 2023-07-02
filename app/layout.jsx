c63c1ea981fcac499fa0038aae7f87f3a979eee5
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-02 07:12:53 +0000

    2 Jul 2023, 12:42

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,10 @@
 import '@styles/globals.css'
 
+export const metadata={
+  title:"Next Demo",
+  description:"Learning the basics of Next.js"
+}
+
 const RootLayout = ({children}) => {
   return (<>
     <div>layout</div>
