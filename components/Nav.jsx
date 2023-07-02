6383465e08b9d4b54844bb6d48a2d17888937937
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-02 08:23:30 +0000

    2 Jul 2023, 13:53

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,12 @@ import { useEffect, useState } from 'react'
 
 const Nav = () => {
   return (
-    <nav className="flex-between w-full mb-16 pt-3">Nav</nav>
+    <nav className="flex-between w-full mb-16 pt-3">
+
+      <Link href='/' className="flex gap-2 flex-center">
+        <Image src='/assets/images/logo.svg'/>
+      </Link>
+    </nav>
   )
 }
 
