6591417d53e80f376d52efcbc0d4fc2e8044b22f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-02 08:01:47 +0000

    2 Jul 2023, 13:31

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section className='w-full flex-center flex-co
-    l'>
+    <section className='w-full flex-center flex-col'>
 
     <h1 className='head_text text-center'>
       Discover and Share
