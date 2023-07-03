427a34f8fcd74e0fe2c102928ae1a15ee016cd9c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-03 17:50:02 +0000

    3 Jul 2023, 23:20

---
+++ components/Test.ts
@@ -0,0 +1,25 @@
+abstract class Test{
+  private  name;
+
+  constructor(name){
+    this.name=name
+  }
+
+  public abstract setName(name);
+  
+}
+
+
+class TestingTest extends Test{
+
+    constructor(name){
+      super(name)
+
+    }
+  public setName(name: any) {
+     this.name=
+  }
+
+
+
+}
