bf23aec319cb43d53a90ff99954fdf166aa8cd3f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-06 08:06:52 +0000

    6 Jul 2023, 13:36

---
+++ app/api/users/route.js
@@ -0,0 +1,14 @@
+export async function GET(request){
+     const users=[
+      {id:1,
+      name:"chiranjeev"
+    },
+     {id:2,
+      name:"zero"
+    },
+
+     ]
+
+
+     return new Response(JSON.stringify(users))
+}
