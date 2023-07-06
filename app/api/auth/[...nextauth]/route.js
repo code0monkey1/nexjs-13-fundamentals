4dcde2a2bbcf36acd3051fb226c98a227baae7ea
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-06 13:34:26 +0000

    6 Jul 2023, 19:04

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,2 +1,12 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
+
+
+const handler = NextAuth({
+  providers:[
+    GoogleProvider({
+      clientId:'',
+      clientSecret:''
+    })
+  ]
+})
