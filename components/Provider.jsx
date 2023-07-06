1d9593533d7d2eb97afdb5876c8ba91442dd14fb
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-06 13:22:44 +0000

    6 Jul 2023, 18:52

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,8 +1,13 @@
+import { SessionProvider } from 'next-auth/react'
 import React from 'react'
 
-const Provider = () => {
+'use client'
+
+const Provider = ({children,session}) => {
   return (
-    <div>Provider</div>
+    <SessionProvider session={session}> 
+       {children}
+    </SessionProvider>
   )
 }
 
