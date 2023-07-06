601efb6b3db7fd9cd7bd0821c5682588b9ae5f8a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-06 08:09:58 +0000

    6 Jul 2023, 13:39

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
 import '@styles/globals.css'
 
 export const metadata={
@@ -14,6 +16,7 @@ const RootLayout = ({children}) => {
       <div className='gradient'/>
       </div>
       <main className='app'>
+        <Nav/>
         {children}
       </main>
 
