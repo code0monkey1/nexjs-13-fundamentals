8a20cced31c589f51960077b8d35fe45bb10ea46
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-08 08:50:01 +0000

    8 Jul 2023, 14:20

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -37,7 +37,7 @@ const Nav = () => {
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-
+     {alert(isUserLoggedIn)}
       <Link href='/' className="flex gap-2 flex-center">
         <Image 
         
