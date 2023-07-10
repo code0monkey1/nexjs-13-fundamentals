5b0307032f01a935bed899a814c6f73272029ea8
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-10 06:24:44 +0000

    10 Jul 2023, 11:54

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -10,10 +10,12 @@ import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useState } from "react"
 
-// export type Propmt={
-//   prompt:string,
-//   tag:string
-// }
+ /* The `CreatePrompt` component is a React component that is responsible for rendering a form for
+filling out a prompt. It uses the `useState` hook to manage the state of the form inputs
+(`prompt` and `tag`). It also uses the `useSession` hook from the `next-auth/react` library to
+get the current logged in client details. The `CreatePrompt` function is currently empty and
+needs to be implemented to handle the form submission. */
+
 const CreatePrompt=()=>{
 
   const [submitting, setSubmitting] = useState(false)
