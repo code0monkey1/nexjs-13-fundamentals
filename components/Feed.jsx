58c0063d8082e360a3de5de97b0d02a75a3141e4
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-12 20:40:28 +0000

    13 Jul 2023, 02:10

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,12 +6,20 @@ const Feed = () => {
    
     const[searchText,setSearchText] = useState('')
 
+    const handleSearchChange=({target})=>{
+
+        setSearchText(target.value)
+    }
+
   return (
     <section className='feed'>
         <form className='relative w-full flex-center'>
           <input
            placeholder='Search for a Tag or a UserName'
            value={searchText}
+           onChange={handleSearchChange}
+           required
+           className='search_input peer'
           />
         </form>
       </section>
