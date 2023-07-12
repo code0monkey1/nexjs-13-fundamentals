77a72fe04e6428242d5dd74efce415363a7042bc
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-12 21:02:21 +0000

    13 Jul 2023, 02:32

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,6 +7,13 @@ import PromptCard from './PromptCard';
 
 const PromptCardList =({data,handleTagClick})=>{
 
+  return (<div className='mt-16 prompt_layout'>
+    
+    {data.map( prompt =><> </>) }
+
+    </div>
+  )
+
 }
 const Feed = () => {
    
