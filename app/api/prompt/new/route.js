c0be77780c004ff485baaa38ff6276f4c9cf27c8
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-12 19:41:37 +0000

    13 Jul 2023, 01:11

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,16 +1,23 @@
-
+import User from "../../../../models/user";
+import { connectToDb } from "../../../../utils/database";
 
 export async function POST(req,res){
+ 
+   try{
+      connectToDb()
 
-  if(!req.body){
+      if(!req.body){
 
-    res.status(501).send("Error : No body found")
-    
-  }
-      
-   if(req.body){
-   
-        
+        res.status(501).send("Error : No body found")
+
+      }
+
    }
+   catch(e){
+
+    res.status(500).send("Error : "+e.message)
+
+   } 
+  
      
 }
