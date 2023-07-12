9ae5447d846777ed4c9bc487e38a1b526c3cad6e
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-12 21:35:19 +0000

    13 Jul 2023, 03:05

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,9 +1,17 @@
 import Prompt from '../../../models/prompt';
+import { connectToDb } from '../../../utils/database';
 export const GET =async()=>{
+  
+  try{
+     
+  await connectToDb()
 
-   
   const prompts= await Prompt.find({})
 
   return new Response(JSON.stringify(prompts),{status:200})
+
+  }catch(e){
+    console.error("Error"+e)
+  }
 
 }
