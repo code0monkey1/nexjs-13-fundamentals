b9f7128e5903ad5dbd5d07c194273498877235d2
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-12 20:02:03 +0000

    13 Jul 2023, 01:32

--- models/prompt.js
+++ models/prompt.js
@@ -4,19 +4,19 @@ const mongoose = require('mongoose'); // Erase if already required
 // Declare the Schema of the Mongo model
 const promptSchema = new mongoose.Schema({
 
+    creator:{
+      type:mongoose.Schema.Types.ObjectId, 
+      ref:'User'
+    },
     prompt:{
         type:String,
-        required:[true,"Prompt is required"],
+        required:[true,"Prompt is required"] ,
     },
     tag:{
         type:String,
         required:[true,"Tag is required"],
         
-    },
-    creator:{
-       type:mongoose.Schema.Types.ObjectId, 
-       ref:'User'
-    },
+    }
 
 });
 
