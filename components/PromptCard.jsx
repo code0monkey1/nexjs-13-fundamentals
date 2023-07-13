a4af018d908ca676fc901a17434559fc95624ffa
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-13 08:12:26 +0000

    13 Jul 2023, 13:42

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -46,7 +46,10 @@ const PromptCard = ({
         </div>
       </div>
       <p className="my-4 font-satoshi text-sm text-gray-700"> {post.prompt}</p>
-      <p className="font-inter text-sm blue_gradient cursor-pointer">
+      <p
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+        className="font-inter text-sm blue_gradient cursor-pointer"
+      >
         #{post.tag}
       </p>
     </div>
