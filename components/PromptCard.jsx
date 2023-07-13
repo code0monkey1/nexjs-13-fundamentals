19db739bf3069c2de7985602b62864062fa2e7aa
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-13 07:05:14 +0000

    13 Jul 2023, 12:35

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,7 +4,16 @@ import { useSession } from "next-auth/react"
 import Image from "next/image"
 import { usePathname, useRouter } from 'next/navigation'
 import { useState } from "react"
-const PromptCard=({key,prompt,handleTagClick})=>{
+
+const PromptCard=(
+  {
+  key,
+  prompt,
+  handleTagClick,
+  handleEdit,
+  handleDelete
+}
+  )=>{
 
   return <>
      <h1>Hello</h1>
