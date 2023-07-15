4572010eaf93105890b0d5baa69223b4d7aaf558
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-15 10:06:46 +0000

    15 Jul 2023, 15:36

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,13 +1,10 @@
 'use client';
+
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { displayCopied } from './Prompt';
 
-/* The `PromptCard` component is a React component that renders a card displaying information
-about a prompt. It receives props such as `post`, `handleTagClick`, `handleEdit`, and
-`handleDelete` to handle different actions related to the prompt. */
 const PromptCard = ({
   key,
   post,
@@ -17,14 +14,6 @@ const PromptCard = ({
 }) => {
   const [copied, setCopied] = useState('');
 
-  /* The `handleCopy` function is responsible for copying the prompt text to the clipboard when
-  the user clicks on the copy button. 
-  
-  It sets the `copied` state to the prompt text, uses the
-  `navigator.clipboard.writeText()` method to copy the text to the clipboard, and then sets
-  the `copied` state back to an empty string after a delay of 3000 milliseconds (3 seconds).
-  
-  This delay is used to change the copied icon back from a ticked icon to the copy icon. */
   const handleCopy = () => {
     setCopied(post.prompt);
 
@@ -34,7 +23,6 @@ const PromptCard = ({
     // to change the copied icon back from ticked
     setTimeout(() => setCopied(''), 3000);
   };
-
   return (
     <div className="prompt_card ">
       <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
@@ -59,7 +47,11 @@ const PromptCard = ({
             width={20}
             height={20}
             alt="copy_image"
-            src={displayCopied(copied, post.prompt)}
+            src={
+              copied === post.prompt
+                ? '/assets/icons/tick.svg'
+                : '/assets/icons/copy.svg'
+            }
           />
         </div>
       </div>
