d9ab5cd53a41632c61e0d5b69e712718d1faeb10
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-15 08:53:15 +0000

    15 Jul 2023, 14:23

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,9 +2,15 @@ import Prompt from '../../../models/prompt';
 import { connectToDb } from '../../../utils/database';
 
 /* `GET` is an asynchronous function that is responsible for fetching prompts from a
-database. It connects to the database, retrieves all prompts, and populates the
-`creator` field of each prompt. It then returns a response with the fetched prompts in
-JSON format and a status code of 200 if successful. If there is an error during the
+database. 
+
+It connects to the database, retrieves all prompts, and populates the
+`creator` field of each prompt. 
+
+It then returns a response with the fetched prompts in
+JSON format and a status code of 200 if successful. 
+
+If there is an error during the
 fetching process, it returns a response with an error message and a status code of 500. */
 
 export const GET = async () => {
