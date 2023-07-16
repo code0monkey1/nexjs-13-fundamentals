24075eb4bd3d079eaaf0a80f769477ba544276d9
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-16 12:57:26 +0000

    16 Jul 2023, 18:27

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,14 +1,71 @@
 'use client';
 
-import { useRouter } from 'next/router';
-import React from 'react';
+// The form for filling out the prompt
+import axios from 'axios';
+import Form from '../../components/Form';
 
-const page = () => {
+//used to get the current logged in client details
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+/* The `CreatePrompt` component is a React component that is responsible for rendering a form for
+filling out a prompt. It uses the `useState` hook to manage the state of the form inputs
+(`prompt` and `tag`). It also uses the `useSession` hook from the `next-auth/react` library to
+get the current logged in client details. The `CreatePrompt` function is currently empty and
+needs to be implemented to handle the form submission. */
+
+const UpdatePrompt = () => {
+  // you get the data object, but you rename it to session to use in your app
+  const { data: session } = useSession();
+
   const router = useRouter();
 
-  console.log('query', router.query);
+  const [submitting, setSubmitting] = useState(false);
 
-  return <div>Edit</div>;
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
+  const updatePrompt = async (e) => {
+    console.log('update prompt triggered');
+    e.preventDefault();
+
+    setSubmitting(true);
+
+    try {
+      const NewPrompt = {
+        prompt: post.prompt,
+        tag: post.tag,
+        userId: session?.user.id,
+      };
+
+      console.log('New Prompt', JSON.stringify(NewPrompt, null, 2));
+
+      const response = await axios.post('/api/prompt/new', NewPrompt);
+
+      if (response.status === 201) {
+        router.push('/');
+      }
+    } catch (e) {
+      console.error('Error: ' + e);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <>
+      <Form
+        type="Update"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </>
+  );
 };
 
-export default page;
+export default UpdatePrompt;
