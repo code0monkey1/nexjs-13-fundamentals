9bc2befcbc5031a3ca247c77090ae14411cad658
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-16 12:58:22 +0000

    16 Jul 2023, 18:28

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -9,14 +9,8 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
-/* The `CreatePrompt` component is a React component that is responsible for rendering a form for
-filling out a prompt. It uses the `useState` hook to manage the state of the form inputs
-(`prompt` and `tag`). It also uses the `useSession` hook from the `next-auth/react` library to
-get the current logged in client details. The `CreatePrompt` function is currently empty and
-needs to be implemented to handle the form submission. */
-
 const UpdatePrompt = () => {
-  // you get the data object, but you rename it to session to use in your app
+  // we need to get the previous data from the saved prompt
   const { data: session } = useSession();
 
   const router = useRouter();
