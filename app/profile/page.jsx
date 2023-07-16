0243cb264cdb07d58e27c53a456155573a9e4ec8
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-16 06:41:18 +0000

    16 Jul 2023, 12:11

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,7 +3,6 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-
 import Profile from '../../components/Profile';
 
 const MyProfile = () => {
