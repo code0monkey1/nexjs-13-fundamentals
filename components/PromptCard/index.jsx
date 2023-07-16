dadf3232d0be2817e4e0d078a838716fbe4c6d66
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-16 07:27:21 +0000

    16 Jul 2023, 12:57

--- components/PromptCard/index.jsx
+++ components/PromptCard/index.jsx
@@ -31,18 +31,17 @@ const PromptCard = ({
     setTimeout(() => setCopied(''), 3000);
   };
 
-  {
-    async () => {
-      console.log(
-        'sessionid',
-        await session?.user.id,
-        'creatorId',
-        post.creator.id,
-        'pathName',
-        pathName
-      );
-    };
-  }
+  (async () => {
+    console.log(
+      'sessionid',
+      await session?.user.id,
+      'creatorId',
+      post.creator.id,
+      'pathName',
+      pathName
+    );
+  })();
+
   return (
     <div className="prompt_card ">
       <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
