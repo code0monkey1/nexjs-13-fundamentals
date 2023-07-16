d36bedf82ec2c216f3c2cb579d9a5917c38c876b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-16 12:51:09 +0000

    16 Jul 2023, 18:21

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,11 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const page = () => {
+  const router = useRouter();
+
+  console.log(router.query);
   return <div>Edit</div>;
 };
 
