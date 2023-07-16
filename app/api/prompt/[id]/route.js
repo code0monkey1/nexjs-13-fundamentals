281a5f7f62ff834fe6a5b3d5ca8e7d25c1a2b58d
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-16 11:54:53 +0000

    16 Jul 2023, 17:24

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -2,4 +2,28 @@ const UPDATE = (req) => {};
 
 const DELETE = (req) => {};
 
-const GET = (req) => {};
+import Prompt from '../../../../models/prompt';
+
+import { connectToDb } from '../../../../utils/database';
+
+export const GET = async (req, { params }) => {
+  // the params will have the `id` property , as this object has all the optional params we pass in a url
+
+  try {
+    await connectToDb();
+
+    /* `const prompts` is declaring a variable that will store the result of the `Prompt.find()` query.
+    The `Prompt.find()` method is used to find all documents in the `Prompt` collection that have a
+    `creator` property matching the `params.id` value. The `populate('creator')` method is used to
+    populate the `creator` field of each prompt with the corresponding user object. The result of
+    the query is then assigned to the `prompts` variable. */
+
+    const prompts = await Prompt.find({ creator: params.id }).populate(
+      'creator'
+    );
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (e) {
+    return new Response('Failed to fetch Prompts : ' + e, { status: 500 });
+  }
+};
