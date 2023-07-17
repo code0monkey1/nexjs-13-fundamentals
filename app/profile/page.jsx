f6f9c0b93d3d25354cac7171f7216bb83e48e66b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-17 13:06:56 +0000

    17 Jul 2023, 18:36

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -68,9 +68,7 @@ const MyProfile = () => {
       {/* This is the reusable profile component that will dynamically render the current users profile info*/}
       <Profile
         name={firstName}
-        desc={`Welcome to ${
-          session ? firstName : ''
-        }'s personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination`}
+        desc={`Welcome to ${firstName}'s personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination`}
         data={myPosts}
         handleEdit={handleEdit}
         handleDelete={handleDelete}
