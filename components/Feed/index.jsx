c64d2c29a18df6bfa63d83c8c58f8e14d58025b6
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-17 12:49:36 +0000

    17 Jul 2023, 18:19

--- components/Feed/index.jsx
+++ components/Feed/index.jsx
@@ -55,12 +55,12 @@ const Feed = () => {
     <section className="feed">
       <form className="relative w-full flex-center">
         <input
+          type="search"
           placeholder="Search for a Tag or a UserName"
           value={searchText}
           onChange={handleSearchChange}
           required
           className="search_input peer"
-          onClick={() => setSearchText('')}
         />
       </form>
       <PromptCardList data={filteredData} handleTagClick={handleTagClick} />
