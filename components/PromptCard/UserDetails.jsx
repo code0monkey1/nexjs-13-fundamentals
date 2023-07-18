5f9fbda8090ee17701c8a37d8d3f8af3372a1f47
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-18 07:47:31 +0000

    18 Jul 2023, 13:17

--- components/PromptCard/UserDetails.jsx
+++ components/PromptCard/UserDetails.jsx
@@ -22,18 +22,6 @@ const UserDetails = ({ post, router, handleCopy }) => {
         </h3>
         <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
       </div>
-      <div className="copy_btn" onClick={handleCopy}>
-        <Image
-          src={
-            copied === post.prompt
-              ? '/assets/icons/tick.svg'
-              : '/assets/icons/copy.svg'
-          }
-          alt={copied === post.prompt ? 'tick_icon' : 'copy_icon'}
-          width={12}
-          height={12}
-        />
-      </div>
     </>
   );
 };
