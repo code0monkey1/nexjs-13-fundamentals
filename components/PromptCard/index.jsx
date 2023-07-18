dea0df09bade49247160d6b375759bd915872bef
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-18 06:59:04 +0000

    18 Jul 2023, 12:29

--- components/PromptCard/index.jsx
+++ components/PromptCard/index.jsx
@@ -31,23 +31,13 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
     <div className="prompt_card ">
       <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-        <Image
-          src={post.creator.image}
-          alt="user_image"
-          width={40}
-          height={40}
-          className="rounded-full object-contain"
-        />
-
         <UserDetails post={post} router={router} />
         <CopyPrompt copied={copied} handleCopy={handleCopy} post={post} />
       </div>
-      <Prompt post={post} />
-      <Tag handleTagClick={handleTagClick} post={post} />
 
-      {/* Display Delete or edit button
-        subjected to the person being validated 
-        to edit or delete prompt*/}
+      <Prompt post={post} />
+
+      <Tag handleTagClick={handleTagClick} post={post} />
 
       {isUserOnProfilePage ? (
         <EditOrDelete handleEdit={handleEdit} handleDelete={handleDelete} />
@@ -103,16 +93,28 @@ const CopyPrompt = ({ copied, handleCopy, post }) => (
   </div>
 );
 
-const UserDetails = ({ post, router }) => (
-  <div className="flex flex-col">
-    <h3
-      onClick={() => {
-        router.push('/');
-      }}
-      className="font-satoshi font-semibold text-gray-900"
-    >
-      {post.creator.username}
-    </h3>
-    <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
-  </div>
-);
+const UserDetails = ({ post, router }) => {
+  return (
+    <>
+      <Image
+        src={post.creator.image}
+        alt="user_image"
+        width={40}
+        height={40}
+        className="rounded-full object-contain"
+      />
+
+      <div className="flex flex-col">
+        <h3
+          onClick={() => {
+            router.push('/');
+          }}
+          className="font-satoshi font-semibold text-gray-900"
+        >
+          {post.creator.username}
+        </h3>
+        <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
+      </div>
+    </>
+  );
+};
