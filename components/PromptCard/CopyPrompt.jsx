7882c41128ad358c0d9d85845a499a705fc9d78e
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-18 08:00:04 +0000

    18 Jul 2023, 13:30

--- components/PromptCard/CopyPrompt.jsx
+++ components/PromptCard/CopyPrompt.jsx
@@ -1,13 +1,12 @@
 import Image from 'next/image';
 const CopyPrompt = ({ post, copied, handleCopy }) => {
+  const displayCopied =
+    copied === prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg';
+
   return (
     <div className="copy_btn" onClick={handleCopy}>
       <Image
-        src={
-          copied === post.prompt
-            ? '/assets/icons/tick.svg'
-            : '/assets/icons/copy.svg'
-        }
+        src={displayCopied}
         alt={copied === post.prompt ? 'tick_icon' : 'copy_icon'}
         width={12}
         height={12}
