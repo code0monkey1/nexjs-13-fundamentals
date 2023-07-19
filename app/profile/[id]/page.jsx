46452d3d7d6b84c170ddb0b191573e5f2c137d13
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-19 12:55:31 +0000

    19 Jul 2023, 18:25

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -23,48 +23,51 @@ const MyProfile = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      // We search for posts by the specific user logged in
- 
-      const response = await axios.get(`/api/users/${params.id}/posts`);
-      const data = response.data;
+      // We search for posts by the specific user with given id
+      try {
+        const response = await axios.get(`/api/users/${params.id}/posts`);
+        const data = response.data;
 
-      setMyPosts(data);
+        setMyPosts(data);
+      } catch (e) {
+        console.error('Invalid User Id');
+      }
     };
 
-    if (session?.user.id) fetchPosts();
-  }, [session?.user.id]);
+    if (params.id) fetchPosts();
+  }, [params.id]);
 
   /* The `handleEdit` function is responsible for handling the editing of a post. When called, it
  navigates the user to the update prompt page for the specific post that is being edited. It uses
  the `router.push` function from the `next/navigation` module to navigate to the update prompt page
  with the post's ID as a query parameter. */
-  const handleEdit = (post) => {
-    router.push(`/update-prompt?id=${post._id}`);
-  };
+  // const handleEdit = (post) => {
+  //   router.push(`/update-prompt?id=${post._id}`);
+  // };
 
   /* The `handleDelete` function is responsible for deleting a post. It first prompts the user to
  confirm if they want to delete the post. If the user confirms, it sends a DELETE request to the
  server to delete the post using the post's ID. After successfully deleting the post, it filters out
  the deleted post from the `myPosts` array and updates the state with the filtered array. */
-  const handleDelete = async (post) => {
-    const hasConfirmed = confirm(
-      'Are you sure you want to delete this prompt?'
-    );
+  // const handleDelete = async (post) => {
+  //   const hasConfirmed = confirm(
+  //     'Are you sure you want to delete this prompt?'
+  //   );
 
-    if (hasConfirmed) {
-      try {
-        await axios.delete(`/api/prompt/${post._id.toString()}`);
+  //   if (hasConfirmed) {
+  //     try {
+  //       await axios.delete(`/api/prompt/${post._id.toString()}`);
 
-        console.log('deleting post');
+  //       console.log('deleting post');
 
-        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+  //       const filteredPosts = myPosts.filter((item) => item._id !== post._id);
 
-        setMyPosts(filteredPosts);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
+  //       setMyPosts(filteredPosts);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  // };
 
   const firstName = session ? `${session?.user.name.split(' ')[0]}'s` : '';
 
@@ -75,8 +78,8 @@ const MyProfile = () => {
         name={firstName}
         desc={`Welcome to ${firstName}'s personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination`}
         data={myPosts}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
+        // handleEdit={handleEdit}
+        // handleDelete={handleDelete}
       />
     </>
   );
