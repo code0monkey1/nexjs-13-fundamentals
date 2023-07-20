020625feaa6c58656251f7de3fd1400d8c41095e
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-20 04:43:06 +0000

    20 Jul 2023, 10:13

--- components/PromptCard/isEditable.js
+++ components/PromptCard/isEditable.js
@@ -1,4 +1,5 @@
 export const isEditable = ({ session, post, pathName, params }) => {
+  console.log('🚀 ~ file: isEditable.js:2 ~ isEditable ~ pathName:', pathName);
   const isAuthorizedUser = session?.user.id === post.creator._id;
   console.log(
     '🚀 ~ file: isEditable.js:3 ~ isEditable ~ isAuthorizedUser:',
@@ -12,7 +13,8 @@ export const isEditable = ({ session, post, pathName, params }) => {
     isOnProfilePage
   );
 
-  const isOnDynamicProfileRoute = isOnProfilePage && params.id;
+  const isOnDynamicProfileRoute = pathName.startsWith('/profile') && params.id;
+
   console.log(
     '🚀 ~ file: isEditable.js:9 ~ isEditable ~ isOnDynamicProfileRoute:',
     isOnDynamicProfileRoute
