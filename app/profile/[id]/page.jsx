aed2a9c8b9c625ac9da4cab47078acf924f9c24c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-20 04:00:28 +0000

    20 Jul 2023, 09:30

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -10,10 +10,8 @@ import Profile from '../../../components/Profile/index';
 the user's profile page. It uses various hooks and functions to fetch and display the user's posts,
 handle editing and deleting of posts, and pass the necessary data and functions to the `Profile`
 component for rendering. */
-const MyProfile = () => {
+const UserProfile = () => {
   const params = useParams();
-
-  const { data: session } = useSession();
 
   const [myPosts, setMyPosts] = useState([]);
 
@@ -35,39 +33,7 @@ const MyProfile = () => {
     if (params.id) fetchPosts();
   }, [params.id]);
 
-  /* The `handleEdit` function is responsible for handling the editing of a post. When called, it
- navigates the user to the update prompt page for the specific post that is being edited. It uses
- the `router.push` function from the `next/navigation` module to navigate to the update prompt page
- with the post's ID as a query parameter. */
-  // const handleEdit = (post) => {
-  //   router.push(`/update-prompt?id=${post._id}`);
-  // };
-
-  /* The `handleDelete` function is responsible for deleting a post. It first prompts the user to
- confirm if they want to delete the post. If the user confirms, it sends a DELETE request to the
- server to delete the post using the post's ID. After successfully deleting the post, it filters out
- the deleted post from the `myPosts` array and updates the state with the filtered array. */
-  // const handleDelete = async (post) => {
-  //   const hasConfirmed = confirm(
-  //     'Are you sure you want to delete this prompt?'
-  //   );
-
-  //   if (hasConfirmed) {
-  //     try {
-  //       await axios.delete(`/api/prompt/${post._id.toString()}`);
-
-  //       console.log('deleting post');
-
-  //       const filteredPosts = myPosts.filter((item) => item._id !== post._id);
-
-  //       setMyPosts(filteredPosts);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-  // };
-
-  const firstName = session ? `${session?.user.name.split(' ')[0]}'s` : '';
+  const firstName = myPosts.length ? myPosts[0].creator.username : '';
 
   console.log('first name is ', firstName);
 
@@ -83,4 +49,4 @@ const MyProfile = () => {
   );
 };
 
-export default MyProfile;
+export default UserProfile;
