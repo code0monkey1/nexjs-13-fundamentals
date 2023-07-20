5c5a9ca96dd4fa5dd6fcc3ae824e4b8a2c71093d
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-20 05:04:45 +0000

    20 Jul 2023, 10:34

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -39,6 +39,38 @@ const UserProfile = () => {
 
   console.log('first name is ', firstName);
 
+  /* The `handleEdit` function is responsible for handling the editing of a post. When called, it
+ navigates the user to the update prompt page for the specific post that is being edited. It uses
+ the `router.push` function from the `next/navigation` module to navigate to the update prompt page
+ with the post's ID as a query parameter. */
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  /* The `handleDelete` function is responsible for deleting a post. It first prompts the user to
+ confirm if they want to delete the post. If the user confirms, it sends a DELETE request to the
+ server to delete the post using the post's ID. After successfully deleting the post, it filters out
+ the deleted post from the `myPosts` array and updates the state with the filtered array. */
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      'Are you sure you want to delete this prompt?'
+    );
+
+    if (hasConfirmed) {
+      try {
+        await axios.delete(`/api/prompt/${post._id.toString()}`);
+
+        console.log('deleting post');
+
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <>
       {/* This is the reusable profile component that will dynamically render the current users profile info*/}
@@ -46,6 +78,8 @@ const UserProfile = () => {
         name={firstName}
         desc={`Welcome to ${firstName}'s personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination`}
         data={myPosts}
+        handleDelete={handleDelete}
+        handleEdit={handleEdit}
       />
     </>
   );
