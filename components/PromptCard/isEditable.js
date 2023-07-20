8f91e89c2bbcad36db3b5406b865f04997288dc4
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-20 04:28:36 +0000

    20 Jul 2023, 09:58

--- components/PromptCard/isEditable.js
+++ components/PromptCard/isEditable.js
@@ -1,9 +1,12 @@
 export const isEditable = ({ session, post, pathName }) => {
   const isAuthorizedUser = session?.user.id === post.creator._id;
 
-  const isOnProfilePage = pathName === '/profile' || pathName === '/profile/*';
+  const isOnProfilePage = pathName === '/profile';
 
-  const isEditable = isAuthorizedUser && isOnProfilePage;
+  const isOnDynamicProfileRoute = isOnProfilePage && router.query.id;
+
+  const isEditable =
+    isAuthorizedUser && (isOnProfilePage || isOnDynamicProfileRoute);
 
   return isEditable;
 };
